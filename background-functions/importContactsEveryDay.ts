32ff5062d993df3c6cd7b056121b09cd82217ee0
gearnode <bryan@frimin.fr>
2023-01-05 18:03:22 +0100

    save wip

---
+++ background-functions/importContactsEveryDay.ts
@@ -0,0 +1,22 @@
+import { defer, init } from "@defer.run/client";
+interface Contact {
+  id: string;
+  name: string;
+}
+
+init({
+  apiUrl: "http://172.16.2.1:8080/api/v1/",
+  debug: true,
+});
+
+const importContactsEveryDay = () => {
+  return new Promise<{ imported: number; companyId: string }>((resolve) => {
+    console.log("start importing contacts");
+    setTimeout(() => {
+      console.log("Done.");
+      resolve({ imported: 10000 });
+    }, 5000);
+  });
+};
+
+export default defer.schedule(importContacts, "every 15 minutes");
