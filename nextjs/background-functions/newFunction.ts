f16f05284f4fab9e5c6f5f1dc1c56de4e20bdb14
Emile Ré <nemile.re@gmail.com>
2023-02-14 16:01:46 +0100

    Add new background function

---
+++ nextjs/background-functions/newFunction.ts
@@ -0,0 +1,35 @@
+import { defer, init } from "@defer.run/client";
+
+type EndState = "started" | "succeed" | "failed"
+
+interface DemoOptions {
+  endState?: EndState
+}
+
+init({
+  // apiUrl: "http://localhost:8080/api/v1/",
+  debug: true,
+});
+
+const newFunction = (options: DemoOptions = {}) => {
+  return new Promise<{ sent: number }>((resolve, reject) => {
+    console.log("Start sending emails");
+
+    setTimeout(() => {
+      switch(options.endState) {
+        case "started":
+          console.log('Running...')
+        case "failed":
+          reject(new Error('fail'));
+          break;
+        case "succeed":
+        default:
+          console.log("Done.");
+          resolve({ sent: 10000 });
+          break;
+      }
+    }, 5000);
+  });
+};
+
+export default defer(newFunction);
