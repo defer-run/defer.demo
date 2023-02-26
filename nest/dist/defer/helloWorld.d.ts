0459dfe2cff72af737738ed2042e1c1372538eea
Charly POLY <cpoly55@gmail.com>
2023-02-25 19:09:40 -0800

    nest demo

---
+++ nest/dist/defer/helloWorld.d.ts
@@ -0,0 +1,2 @@
+declare const _default: import("@defer/client").DeferRetFn<() => Promise<unknown>>;
+export default _default;
---
+++ nest/src/defer/helloWorld.ts
@@ -0,0 +1,11 @@
+import { defer } from '@defer/client';
+
+const helloWorld = () =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Hello World!`);
+      resolve('done');
+    }, 5000);
+  });
+
+export default defer(helloWorld);
