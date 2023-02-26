cd11c304cd31eec94bbb637387c44039eac44f87
Charly POLY <cpoly55@gmail.com>
2023-02-25 18:27:36 -0800

    koa demo

---
+++ koa/packages/api/index.ts
@@ -0,0 +1,15 @@
+import Koa from "koa";
+
+// we import our `helloWorld()` background function
+import { helloWorld } from "defer";
+
+const app = new Koa();
+
+app.use(async (ctx) => {
+  // calling a background function triggers an execution on Defer Platform
+  await helloWorld("Charly");
+
+  ctx.body = "Hello World";
+});
+
+app.listen(3000);
