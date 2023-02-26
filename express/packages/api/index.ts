553f9589db0306be8761fa6e09331956522bf0bb
Charly POLY <cpoly55@gmail.com>
2023-02-25 18:17:11 -0800

    express demo

---
+++ express/packages/api/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+// we import our `helloWorld()` background function
+import { helloWorld } from "defer";
+
+const app = express();
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  // calling a background function triggers an execution on Defer Platform
+  await helloWorld("Charly");
+
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
