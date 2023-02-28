0794e906ce567398e7ff0bae3d3305d144ffcc10
Charly POLY <cpoly55@gmail.com>
2023-02-27 17:22:39 -0800

    fix(sveltekit): remove patches

--- sveltekit/src/defer/helloWorld.ts
+++ sveltekit/src/defer/helloWorld.ts
@@ -1,10 +1,14 @@
 import { defer } from '@defer/client';
 
 import { getName } from '$lib/utils/getName';
+import { DEFER_TOKEN } from '$env/static/private';
 
 const helloWorld = () =>
 	new Promise((resolve) => {
 		setTimeout(() => {
+			if (DEFER_TOKEN) {
+				console.log('Defer.');
+			}
 			console.log(`Hello ${getName()}!`);
 			if (import.meta.vitest) {
 				console.log('in testing');
