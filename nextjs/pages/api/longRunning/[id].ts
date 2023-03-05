662bd7c937cb0247ca4a1acbe4b255b977c07e77
Charly POLY <cpoly55@gmail.com>
2023-03-04 21:17:12 -0800

    feat(nextjs): polling for execution status example

---
+++ nextjs/pages/api/longRunning/[id].ts
@@ -0,0 +1,15 @@
+import { type FetchExecutionResponse, getExecution } from "@defer/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Response = {
+  res: FetchExecutionResponse;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response>
+) {
+  const executionId = req.query.id;
+  const ret = await getExecution(executionId as string);
+  res.status(200).json({ res: ret });
+}
